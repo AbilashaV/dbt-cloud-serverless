acd469971399b60356288419d1d8207c6d0c2e47
Abilasha Vediappan <abi.vedhi@gmail.com>
2024-06-04 06:13:45 +0000

    changes

--- models/cleansed/c_credit_card_entity.sql
+++ models/cleansed/c_credit_card_entity.sql
@@ -1,14 +1,16 @@
---{{config(materialized='table')}}
+{{config(materialized='table')}}
 
 Select 
-id as credit_card_id,
+cce.id as credit_card_id,
 card_brand,
 --card_last4,
-country,
-created_at as created_at_utc,
+cce.country,
+cce.created_at::timestamp as created_at_utc,
+(cce.created_at::timestamp+ interval '1 hour' * rg.timezone) as created_at_local,
 customer_id,
 default_selected,
-deleted_at as deleted_at_utc, 
+deleted_at::timestamp as deleted_at_utc, 
+(deleted_at::timestamp+ interval '1 hour' * rg.timezone) as deleted_at_local,
 exp_month,
 exp_year,
 --fingerprint,
@@ -17,7 +19,8 @@ gateway,
 issuer,
 --source_id?
 --threedsecured?,
-updated_at as updated_at_utc,
+cce.updated_at::timestamp as updated_at_utc,
+(cce.updated_at::timestamp+ interval '1 hour' * rg.timezone) as updated_at_local,
 --user_id
 user_uuid,
 uuid
@@ -26,4 +29,5 @@ uuid
 --auth_flow_type
 --bin
 --corporate_id
-from {{ ref("src_credit_card_entity")}}
+from {{ ref("src_credit_card_entity")}} cce
+join {{ ref("regions") }} rg on cce.country = rg.country
--- models/cleansed/c_ride_entity.sql
+++ models/cleansed/c_ride_entity.sql
@@ -1,6 +1,7 @@
--- {{config(materialized='table')}}
+{{config(materialized='table')}}
+
 select
-    id as ride_id,
+    r.id as ride_id,
     assign_time::timestamp as assign_time_utc,
     (assign_time::timestamp + interval '1 hour' * rg.timezone) as assign_time_local,
     cancel_reason,
@@ -10,20 +11,28 @@ select
     company_id,
     coupon_code,
     create_time::timestamp as create_time_utc,
+    (create_time::timestamp+ interval '1 hour' * rg.timezone) as create_time_local,
     credit_card_id,
     currency,
     discount,
     driver_application_fee,
     driver_info,
     drop_off_time::timestamp as drop_off_time_utc,
+    (drop_off_time::timestamp + interval '1 hour' * rg.timezone) as drop_off_time_local,
     estimated_price,
     etc_fee,
     pay_status,
     payment_method,
     pick_up_arrived_time::timestamp as pick_up_arrived_time_utc,
+    (
+        pick_up_arrived_time::timestamp + interval '1 hour' * rg.timezone
+    ) as pick_up_arrived_time_local,
     -- pick_up_distance_km,
     trunc(pick_up_distance_km, 2) as pick_up_distance_km,
     pick_up_start_time::timestamp as pick_up_start_time_utc,
+    (
+        pick_up_start_time::timestamp + interval '1 hour' * rg.timezone
+    ) as pick_up_start_time_local,
     pick_up_zone_id,
     refund_amount,
     region,
@@ -36,6 +45,7 @@ select
     rider_uuid,
     sd,
     start_time::timestamp as start_time_utc,
+    (start_time::timestamp + interval '1 hour' * rg.timezone) as start_time_local,
     toll_fee,
     way_point,
     fraud,
@@ -65,6 +75,9 @@ select
     rider_cancellation_reward,
     driver_cancellation_reward,
     reservation_ride_start_time::timestamp as reservation_ride_start_time_utc,
+    (
+        reservation_ride_start_time::timestamp + interval '1 hour' * rg.timezone
+    ) as reservation_ride_start_time_local,
     reservation_fee,
     driver_penalty_fee,
     rider_system_fee_tax,
@@ -74,6 +87,7 @@ select
     pick_up_h3_res15,
     destination_h3_res15,
     confirm_time::timestamp as confirm_time_utc,
+    (confirm_time::timestamp + interval '1 hour' * rg.timezone) as confirm_time_local,
     payment_item_uuid,
     mdd,
     rider_penalized_amount
