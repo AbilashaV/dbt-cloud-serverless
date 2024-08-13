682562a27bdec44c26db6e58747bd24dc855a741
AbilashaV <41884474+AbilashaV@users.noreply.github.com>
2024-08-13 13:32:44 +0800

    Update stg_ride_entity.sql

--- models/staging/stg_ride_entity.sql
+++ models/staging/stg_ride_entity.sql
@@ -1,7 +1,8 @@
 SELECT
     *
 FROM
-    {{source('ride_','ride_entity_0619')}}
+    --{{source('ride_','ride_entity_0619')}}
+    "dev"."tada_ride_service"."ride_entity"
 --WHERE 
     --EXTRACT(YEAR FROM create_time::timestamp) = 2024
     --DATE_TRUNC('day', CAST(create_time AS timestamp)) >= '2024-05-01 00:00:00' AND  
